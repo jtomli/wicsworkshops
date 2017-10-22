949bf0f7e338f706f389a7c6161e5dfe124b81ea
jtomli <jtomli@seas.upenn.edu>
2017-10-22 18:30:47 -0400

    comments and updates

--- workshop1/chatApp/jquery-client.js
+++ workshop1/chatApp/jquery-client.js
@@ -1,19 +1,27 @@
+// function waits for jQuery to be loaded onto browser
 $(document).ready(function() {
+
+  // function to format a message
   var formatMessage = function(user, message) {
-    return "<span> " + user + " : " + message + " </span>";
+    return "<span style=color:'blue'>" + user + " : " + message + " </span>";
   };
 
-  var postMessage = function(contents) {
-    var message = "<li> " + contents + " </li>";
-    $("#message-list").append(message);
+  // function to post our message to list
+  var postMessage = function(color, contents) {
+    var message = "<li><span style=color:'" + color + "'>" + contents + "</span> </li>";
+    // accesses a "virtual element" (does not exist yet)
+    // hides then fades in for visual effect, appending to message-list
+    $(message).hide().appendTo("#message-list").fadeIn(200);
   };
 
+  // message has been submitted
   $("#message-form").submit(function(event) {
+    // prevent browser from controlling submit
     event.preventDefault();
 
+    // value of our message-box
     var message = $("#message-box").val();
-    postMessage(formatMessage("Jamie", message));
-
+    postMessage("black", formatMessage("Jamie", message));
     $("#message-box").val("");
   });
 });
